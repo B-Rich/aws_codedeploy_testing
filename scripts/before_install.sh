f619017cc9175717185bef36b3c7f21178a1380a
Colin Johnson <colinbjohnson@gmail.com>
2016-04-14 19:31:52 +0000

    resolve issue with copy command having failed

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # copy files into place
 DEPLOYMENT_ROOT='/opt/codedeploy-agent/deployment-root'
-cp ${DEPLOYMENT_ROOT}/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive/public /var/www/${DEPLOYMENT_ID}
+cp -r ${DEPLOYMENT_ROOT}/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive/public /var/www/${DEPLOYMENT_ID}
