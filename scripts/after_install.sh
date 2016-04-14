3b9c732b3280b075571892b2e6bc4e227b110a5e
Colin Johnson <colinbjohnson@gmail.com>
2016-04-14 19:10:55 +0000

    update to test of deploy, symlink and restart

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+ln -sfn "/var/www/$DEPLOYMENT_ID" /var/www/public
 service apache2 restart
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-service apache2 stop
-rm -rf /var/www/html
+# copy files into place
+DEPLOYMENT_ROOT='/opt/codedeploy-agent/deployment-root'
+cp ${DEPLOYMENT_ROOT}/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive/public /var/www/${DEPLOYMENT_ID}
